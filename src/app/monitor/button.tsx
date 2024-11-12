7059becc926166ffc07d3746b65e442628473950
choisuyeon <stellasue@naver.com>
2024-11-12 20:02:58 +0900

    feat: Add camera function in monitor/page.tsx

---
+++ src/app/monitor/button.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+interface ChooseFuncProps {
+    srcS: string;
+    altS: string;
+    widthS: number | `${number}`;
+    heightS: number | `${number}`;
+    classS1: string;
+    classS2: string;
+    textS: string;
+    onClick?: () => void;
+}
+
+export default function ChooseFunc({ srcS, altS, widthS, heightS, classS1, classS2, textS, onClick }: ChooseFuncProps ) {
+    return (
+        <div 
+            className="flex justify-around bg-white w-[230px] h-[50px] rounded-2xl"
+            onClick={onClick}
+        >
+            <Image src={srcS} alt={altS} width={widthS} height={heightS} className={classS1}></Image>
+            <div className={`flex items-center ${classS2}`}>{textS}</div>
+        </div>
+    )
+}
