f0ac2972ef813df39ed1f3eb3ba5ba0a67ba3794
KNU-K <knukang334@gmail.com>
2024-11-15 03:20:43 +0900

    refactor:  fix to uploading Data feature

--- src/app/api/detect/service/detector.ts
+++ src/app/api/detect/service/detector.ts
@@ -3,9 +3,10 @@ import FormData from "form-data";
 import { Response } from "../types";
 
 class DetectorService {
-    async analysis(imageUrl: string, confidence: string, nmsThreshold: string): Promise<Response> {
-        const imageBuffer = await this.fetchImageData(imageUrl);
-        const formData = this.createFormData(imageBuffer, confidence, nmsThreshold);
+    async analysis(imageBuffer: ArrayBuffer, confidence: string, nmsThreshold: string): Promise<Response> {
+        const buf = await this.fetchImageData(imageBuffer);
+        const formData = this.createFormData(buf, confidence, nmsThreshold);
+        console.log("a");
         const response = await axios.post("http://34.22.74.167:5000/process_image", formData, {
             headers: {
                 "Content-Type": "multipart/form-data",
@@ -15,13 +16,13 @@ class DetectorService {
         console.log(response.data);
         return response.data;
     }
-    private async fetchImageData(imageUrl: string): Promise<Buffer> {
-        const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
-        return Buffer.from(response.data);
+    private async fetchImageData(imageBuffer: ArrayBuffer): Promise<Buffer> {
+        // const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+        return Buffer.from(imageBuffer);
     }
-    private createFormData(imageBuffer: Buffer, confidence: string, nmsThreshold: string) {
+    private createFormData(buf: Buffer, confidence: string, nmsThreshold: string) {
         const formData = new FormData();
-        formData.append("image", imageBuffer, "image.jpg");
+        formData.append("image", buf, "image.jpg");
         formData.append("confidence", confidence);
         formData.append("nms_threshold", nmsThreshold);
         return formData;
