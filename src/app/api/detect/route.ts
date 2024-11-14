318b25f295ebb72b6eac9b1f27ab648d191afd3d
KNU-K <knukang334@gmail.com>
2024-11-14 19:43:43 +0900

    refactor : optimize route with detecting the solar panel

--- src/app/api/detect/route.ts
+++ src/app/api/detect/route.ts
@@ -1,87 +1,70 @@
 "use server";
 import { NextResponse } from "next/server";
 import axios from "axios";
-import FormData from "form-data";
 import prisma from "@/lib/prisma";
+import { Decimal } from "@prisma/client/runtime/library";
+import s3Util from "../../utils/s3";
+import detectorService from "./service/detector";
+import Formatter from "./service/formatter";
 
-interface Detection {
-    box: { x1: number; x2: number; y1: number; y2: number };
-    confidence: number;
-    label: string;
-}
-
-function validateInput(imageUrl: string | undefined) {
-    if (!imageUrl) {
-        throw new Error("Image URL is required");
-    }
-}
-
-async function fetchImageData(imageUrl: string) {
-    const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
-    return Buffer.from(response.data);
-}
-
-function createFormData(imageBuffer: Buffer, confidence: string, nmsThreshold: string) {
-    const formData = new FormData();
-    formData.append("image", imageBuffer, "image.jpg");
-    formData.append("confidence", confidence);
-    formData.append("nms_threshold", nmsThreshold);
-    return formData;
-}
-
-async function sendImageForProcessing(formData: FormData) {
-    const response = await axios.post("http://34.22.74.167:5000/process_image", formData, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-            ...formData.getHeaders(),
-        },
-    });
-    return response.data.detections;
-}
-
-function formatDetectionData(imageUrl: string, detections: Detection[]) {
-    return detections.map((detection) => ({
-        imageUrl,
-        x1: detection.box.x1,
-        x2: detection.box.x2,
-        y1: detection.box.y1,
-        y2: detection.box.y2,
-        confidence: detection.confidence,
-        label: detection.label,
-    }));
-}
 /**
  * @description AI 사용
  */
+
 // Main handler function
 export async function POST(req: Request) {
     try {
         const { imageUrl, confidence = "0.5", nmsThreshold = "0.3" } = await req.json();
 
-        validateInput(imageUrl);
+        if (!imageUrl) {
+            throw new Error("Image URL is required");
+        }
 
-        const imageBuffer = await fetchImageData(imageUrl);
-        const formData = createFormData(imageBuffer, confidence, nmsThreshold);
-
-        const detections = await sendImageForProcessing(formData);
-        const formattedData = formatDetectionData(imageUrl, detections);
-
+        const { detections, processed_image } = await detectorService.analysis(imageUrl, confidence, nmsThreshold);
+        const s3Url = await s3Util.upload(processed_image);
+        const formattedData = Formatter.formatDetectionData(s3Url, detections);
         await prisma.imageDetection.createMany({ data: formattedData });
-
-        return NextResponse.json(detections);
+        return NextResponse.json(formattedData);
     } catch (error) {
         console.error("Error:", error);
         return NextResponse.json({ message: error || "Error processing the image" });
     }
 }
 
-export async function GET() {
+export async function GET(req: Request) {
+    const url = new URL(req.url);
+    const startDate = url.searchParams.get("startDate");
+    const endDate = url.searchParams.get("endDate");
+
+    const filters: { createdAt?: { gte?: Date; lte?: Date } } = {};
+
+    if (startDate) {
+        filters.createdAt = {
+            ...filters.createdAt,
+            gte: new Date(startDate),
+        };
+    }
+
+    if (endDate) {
+        filters.createdAt = {
+            ...filters.createdAt,
+            lte: new Date(endDate),
+        };
+    }
+
     try {
-        const detections = await prisma.imageDetection.findMany();
-        return NextResponse.json(detections);
+        const detections = await prisma.imageDetection.findMany({
+            where: filters,
+        });
+
+        if (detections.length === 0) {
+            return NextResponse.json({ message: "No detections found" });
+        }
+
+        return NextResponse.json({ detections });
     } catch (error) {
         console.error("Error fetching detections:", error);
-        return NextResponse.json({ message: "Failed to fetch detections" });
+        return NextResponse.json({ message: "Failed to fetch detections", error: error });
     } finally {
         await prisma.$disconnect();
     }
