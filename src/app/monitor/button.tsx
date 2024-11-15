63fe6272c88da22f0932ed9ae2474c7a7be2b0ff
kimgho <kgh67811@naver.com>
2024-11-16 02:18:35 +0900

    feat(monitor): add loading and video snapshot

--- src/app/monitor/button.tsx
+++ src/app/monitor/button.tsx
@@ -11,15 +11,15 @@ interface ChooseFuncProps {
     onClick?: () => void;
 }
 
-export default function ChooseFunc({ srcS, altS, widthS, heightS, classS1, classS2, textS, onClick }: ChooseFuncProps ) {
+export default function ChooseFunc({ srcS, altS, widthS, heightS, classS1, classS2, textS, onClick }: ChooseFuncProps) {
     return (
-        <div 
+        <div
             className="flex justify-around bg-white-green w-[230px] h-[50px] rounded-2xl outline-double outline-4 outline-light-green"
             onClick={onClick}
         >
             <Image src={srcS} alt={altS} width={widthS} height={heightS} className={classS1}></Image>
             <div className={`flex items-center ${classS2}`}>{textS}</div>
-            
+
         </div>
     )
 }
